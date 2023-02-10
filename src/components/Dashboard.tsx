ba0b043bed11b0172b1d112004bf34e3e39a9ac4
Haoqi7 <w00989988@gmail.com>
2023-02-10 10:37:47 +0800

    Update Dashboard.tsx

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -40,10 +40,9 @@ const Dashboard = () => {
     <div className="dashboard-wrapper" onDoubleClick={handleDashBoardDoubleClick}>
       <div className="hint-block" onDoubleClick={handleStopPropagation}>
         <img className="logo-img" src="/sticky-notes.ico" alt="" />
-        <span>double-click on any blank space to create a card. </span>
-        <a href="https://github.com/justmemos/sticky-notes">
-          <img src="https://img.shields.io/github/stars/usememos/sticky-notes" alt="" />
-        </a>
+        <span>双击任何空白区域创建卡片。
+          <p>double-click on any blank space to create a card. </p></span>
+
       </div>
       <Toolbar />
       {noteList.map((note) => {
